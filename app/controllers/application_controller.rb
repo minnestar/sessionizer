3910be0cca7961bcfaf3ecb4ab9f27a73d14d96d
Luke Francl <look@recursion.org>
2010-04-23 16:44:16 -0500

    remember participants

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,18 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   helper :layout
+  helper_method :current_user
+  helper_method :logged_in?
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  def current_participant
+    @current_user ||= Participant.find_by_id(session[:participant_id])
+  end
+
+  def logged_in?
+    session[:participant_id] && !current_participant.nil?
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,19 @@ class SessionsController < ApplicationController
     actions :index, :show, :new, :create, :update
 
     before :create do
-      name, email = object_parameters[:name], object_parameters[:email]
+      if logged_in?
+        current_object.participant = current_participant
+      else
+        name, email = object_parameters[:name], object_parameters[:email]
 
-      participant = Participant.first(:conditions => {:email => email}) || Participant.create(:email => email, :name => name)
-      if !participant.new_record?
-        current_object.participant = participant
+        participant = Participant.first(:conditions => {:email => email}) || Participant.create(:email => email, :name => name)
+      
+        if !participant.new_record?
+          session[:participant_id] = participant.id
+          current_object.participant = participant
+        else
+          current_object.errors.add_to_base("The participant is invalid.") # FIXME: better message
+        end
       end
     end
   end
