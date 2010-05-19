8a47087dc7ffcf81bd9d111f6a75c4efe26577c7
Luke Francl <look@recursion.org>
2010-05-19 10:18:11 -0500

    add login functionality

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,26 @@
+class UserSessionsController < ApplicationController
+  def new
+    @participant = Participant.new
+  end
+
+  def create
+    @participant = Participant.first(:conditions => {:email => params[:participant][:email]})
+
+    if !verify_recaptcha
+      @participant = Participant.new(:email => params[:participant][:email])
+      flash[:error] = "Sorry, your recaptcha response wasn't reccognized. Please try again."
+      render :action => 'new'
+      return
+    end
+
+    if @participant
+      session[:participant_id] = @participant.id
+      flash[:notice] = "You're logged in. Welcome back."
+      redirect_to root_path
+    else
+      @participant = Participant.new(:email => params[:participant][:email])
+      flash[:error] = "Sorry, couldn't find that participant. Try again, or sign up to attend a session to register a new account."
+      render :action => 'new'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ ActionController::Routing::Routes.draw do |map|
     session.resource :attendance, :only => [:create]
   end
   map.resources :participants, :only => [:show, :edit, :update]
+
+  map.new_login '/login', :controller => 'user_sessions', :action => 'new', :conditions => {:method => :get}
+  map.login '/login', :controller => 'user_sessions', :action => 'create', :conditions => {:method => :post}
 end
