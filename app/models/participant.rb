22be6a4fcba9b48806a3d4f1142684e096ee1dca
Luke Francl <look@recursion.org>
2010-05-19 21:22:40 -0500

    allow users to see which sessions they're attending

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,5 +1,7 @@
 class Participant < ActiveRecord::Base
   has_many :sessions
+  has_many :attendances
+  has_many :sessions_attending, :through => :attendances, :source => :session
 
   validates_presence_of :name
   validates_uniqueness_of :email, :case_sensitive => false
