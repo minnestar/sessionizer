a241be9e7a5130b4ee0c07b2afc3458348c42493
Luke Francl <look@recursion.org>
2010-05-21 17:05:43 -0500

    re-enable session reg temporarily

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,32 +5,32 @@ class SessionsController < ApplicationController
     actions :index, :show, :new, :edit, :update
   end
 
-  # def create
-  #   build_object
-  #   load_object
+  def create
+    build_object
+    load_object
     
-  #   if logged_in?
-  #     current_object.participant = current_participant
-  #   else
-  #     name, email = object_parameters[:name], object_parameters[:email]
+    if logged_in?
+      current_object.participant = current_participant
+    else
+      name, email = object_parameters[:name], object_parameters[:email]
 
-  #     participant = Participant.first(:conditions => {:email => email}) || Participant.create(:email => email, :name => name)
+      participant = Participant.first(:conditions => {:email => email}) || Participant.create(:email => email, :name => name)
       
-  #     if !participant.new_record?
-  #       session[:participant_id] = participant.id
-  #       current_object.participant = participant
-  #     else
-  #       current_object.errors.add_to_base("The participant is invalid.") # FIXME: better message
-  #     end
-  #   end
+      if !participant.new_record?
+        session[:participant_id] = participant.id
+        current_object.participant = participant
+      else
+        current_object.errors.add_to_base("The participant is invalid.") # FIXME: better message
+      end
+    end
 
-  #   if verify_recaptcha(:model => current_object, :message => "Please try entering the captcha again.") && current_object.save
-  #     flash[:notice] = "Thanks for adding your session."
-  #     redirect_to current_object
-  #   else
-  #     render :action => 'new'
-  #   end
-  # end
+    if verify_recaptcha(:model => current_object, :message => "Please try entering the captcha again.") && current_object.save
+      flash[:notice] = "Thanks for adding your session."
+      redirect_to current_object
+    else
+      render :action => 'new'
+    end
+  end
 
   STOP_WORDS = Set.new(['session', 'etc', 'just', 'presentation', 'get', 'discussion'])
 
