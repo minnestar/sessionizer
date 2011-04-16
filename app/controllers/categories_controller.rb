18f2818dc1558db771fc1a9c864112e0644db77c
Luke Francl <look@recursion.org>
2011-04-16 08:35:59 -0500

    only show current event sessions on category list page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
-    @sessions = @category.sessions
+    @sessions = @category.sessions.for_current_event
   end
 end
