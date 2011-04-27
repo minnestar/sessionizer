aa1e10084f7526bbbbd4884ba5363a3d640c3706
Luke Francl <look@recursion.org>
2011-04-27 09:35:41 -0500

    fix problem with named_scope. Need to use all to add conditions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @current_event = Event.current_event
     
-    @recent_sessions = @current_event.sessions(:limit => 10, :order => 'created_at desc')
+    @recent_sessions = @current_event.sessions.all(:limit => 10, :order => 'created_at desc')
     @development = Category.find_by_name('Development')
     @design = Category.find_by_name('Design')
     @hardware = Category.find_by_name('Hardware')
