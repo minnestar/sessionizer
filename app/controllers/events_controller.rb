5ae7c4025cc3ea9c0102edb89962ad9b3df4804f
Luke Francl <look@recursion.org>
2012-03-04 10:58:39 -0600

    add JSON export for events with sessions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,5 +5,11 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id], :include => :sessions)
+    respond_to do |format|
+      format.html
+      format.json do
+        render :json => @event.to_json(:include => { :sessions => { :include => { :participant => { :except => :email } } } } )
+      end
+    end
   end
 end
