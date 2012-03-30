7d3be67388f8312eef6116638eab8e377051c798
Paul Cantrell <cantrell@pobox.com>
2012-03-30 11:25:13 -0500

    Presenters attend their own sessions.

--- lib/scheduling/schedule.rb
+++ lib/scheduling/schedule.rb
@@ -1,4 +1,5 @@
 require 'set'
+require 'pp'
 
 
 unless Array.method_defined?(:sample)
@@ -15,12 +16,12 @@ module Scheduling
     
     def self.build_sets(sessions, association_class)
       # This brute force iteration is hardly slick, but I'm too rusty on fancy querying in Rails 2.3 to care just now. -PPC
-      by_participant = Hash.new { |h,k| h[k] = [] }
+      session_set = Hash.new { |h,k| h[k] = [] }
       association_class.find(:all, :conditions => { :session_id => sessions }, :select => 'participant_id, session_id').each do |assoc|
-        by_participant[assoc.participant_id] << assoc.session_id
+        session_set[assoc.participant_id] << assoc.session_id
       end
-      puts "#{by_participant.count} #{association_class.name.pluralize.humanize.downcase}"
-      by_participant.values
+      puts "#{session_set.count} #{association_class.name.pluralize.humanize.downcase}"
+      session_set
     end
   
     def initialize(*args)
@@ -29,8 +30,15 @@ module Scheduling
         @sessions = event.session_ids
         @time_slots = event.timeslots
         @max_per_slot = event.rooms.count
+        
         @attendance_sets = Schedule.build_sets @sessions, Attendance
         @presenter_sets  = Schedule.build_sets @sessions, Presentation
+        
+        # Presenters also attend their own sessions:
+        @presenter_sets.each do |person, sessions|
+          @attendance_sets[person] += sessions
+          @attendance_sets[person].uniq!
+        end
       elsif args.length == 5
         @sessions, @time_slots, @max_per_slot, @attendance_sets, @presenter_sets = *args
       else
@@ -122,7 +130,7 @@ module Scheduling
       score = 0
       slots_used = Set.new
       
-      session_sets.each do |set|
+      session_sets.each do |person, set|
         next if set.empty?
         overlaps = 0
         set.each do |session|
@@ -139,34 +147,3 @@ module Scheduling
   end
 end
 
-
-
-
-
-# sched = Scheduling::Schedule.new(
-#   %w(soup nuts penguins walruses monkeys chickens ocelots snowboarding windsurfing bobsledding glossolalia),
-#   %w(9:00 10:00 11:00 12:00),
-#   4,
-#   [%w(soup nuts),
-#    %w(penguins walruses monkeys chickens),
-#    %w(snowboarding windsurfing),
-#    %w(glossolalia nuts monkeys),
-#    %w(soup nuts penguins walruses monkeys chickens ocelots snowboarding windsurfing bobsledding glossolalia),
-#    %w(windsurfing walruses glossolalia ocelots),
-#    %w(windsurfing monkeys),
-#    %w(windsurfing nuts penguins),
-#    %w(snowboarding ocelots monkeys),
-#    %w(nuts ocelots chickens),
-#    %w(snowboarding glossolalia),
-#    %w(chickens soup),
-#    %w(penguins soup),
-#    %w(snowboarding walruses nuts)],
-#   [%w(penguins snowboarding bobsledding),
-#    %w(penguins ocelots)])
-# 
-# p sched
-# 
-# annealer = Scheduling::Annealer.new(:max_iter => 80000, :cooling_time => 10000000, :repetition_count => 3)
-# best = annealer.anneal(sched)
-# puts "BEST SOLUTION:"
-# p best
