71ead4f9d58c26013ad4ed93f760599463c2cd51
Luke Francl <look@recursion.org>
2012-04-06 10:20:45 -0500

    sort by sessions by room capacity, then name

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,6 +1,13 @@
 module SchedulesHelper
   def session_columns_for_slot(slot, num_cols = 2, &block)
-    sessions = slot.sessions.sort_by { |s| -s.room.capacity }
+    sessions = slot.sessions.sort do |s1, s2|
+      capacity = -(s1.room.capacity <=> s2.room.capacity)
+      if capacity == 0
+        s1.room.name <=> s2.room.name
+      else
+        capacity
+      end
+    end
     split = sessions.length / 2 + 1
     yield sessions[0...split]
     yield sessions[split..-1]
