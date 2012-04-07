33fb4fdec72a3756a07604fbc9e30d768fb8b545
Luke Francl <look@recursion.org>
2012-04-07 09:39:41 -0500

    throw exception for debugging

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -12,7 +12,7 @@ class Admin::SessionsController < Admin::AdminController
     presenter = Participant.find_by_name(params[:session][:name])
 
     unless presenter
-      presenter = Participant.create(:name => params[:session][:name])
+      presenter = Participant.create!(:name => params[:session][:name])
     end
 
     @session = Event.current_event.sessions.new(params[:session])
@@ -20,7 +20,7 @@ class Admin::SessionsController < Admin::AdminController
     @session.timeslot_id = params[:session][:timeslot_id]
     @session.room_id = params[:session][:room_id]
     
-    if @session.save
+    if @session.save!
       flash[:notice] = "Presentation added"
       redirect_to admin_sessions_path
     else
