e483b7424e415432bb39fde25e6f5cf605d6fddd
Luke Francl <look@recursion.org>
2012-04-07 01:39:36 -0500

    force SSL for admin; update password

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,12 +1,19 @@
 class Admin::AdminController < ApplicationController
+  before_filter :redirect_to_ssl
   before_filter :authenticate
 
   private
 
+  def redirect_to_ssl
+    if Rails.env.production? && !request.ssl?
+      redirect_to "https://sessionizer.heroku.com/admin/sessions/new"
+    end
+  end
+
   def authenticate
     if Rails.env.production?
       authenticate_or_request_with_http_basic do |user_name, password|
-        user_name == "minnestar" && password == "nottheusualpassword"
+        user_name == "sessionizer" && password == "SuperDuperSecure!"
       end
     end
   end
