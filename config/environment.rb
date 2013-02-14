71ee02e1d85836188cfdca24058c8c2f8b56b782
Casey Helbling <casey@softwareforgood.com>
2013-02-13 21:02:14 -0600

    Upgraded rails version to 2.3.17 Added a .rbenv-version

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.17' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -16,6 +16,7 @@ Rails::Initializer.run do |config|
   config.gem 'dalli'
   config.gem 'fastercsv'
   config.gem 'icalendar'
+
 
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
