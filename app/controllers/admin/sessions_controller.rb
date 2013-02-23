e64d8b4dccb8784a5f3e66173b12e00097d80dfb
Casey Helbling <casey@softwareforgood.com>
2013-02-23 17:47:56 -0600

    More work on the admin sessions area

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,20 +2,29 @@ class Admin::SessionsController < Admin::AdminController
   make_resourceful do
     actions :index, :edit, :update, :new, :create
 
-    response_for :update do
+    #response_for :update do
+      #redirect_to admin_sessions_path
+    #end
+  end
+  
+  def update
+    if current_object.update_attributes(params[:session], :without_protection => true)
       redirect_to admin_sessions_path
+      
+    else
+      render :edit
     end
   end
 
   def create
     # find exact match by name
-    presenter = Participant.find_by_name(params[:session][:name])
+    presenter = Participant.where(:name => params[:session][:name]).first
 
     unless presenter
       presenter = Participant.create(:name => params[:session][:name])
     end
 
-    @session = Event.current_event.sessions.new(params[:session])
+    @session = Event.current_event.sessions.new(params[:session], :without_protection => true)
     @session.participant = presenter
     @session.timeslot_id = params[:session][:timeslot_id]
     @session.room_id = params[:session][:room_id]
