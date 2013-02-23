6eb4aa89b4669f093426e74a18e4374c5686ae68
Casey Helbling <casey@softwareforgood.com>
2013-02-23 17:58:22 -0600

    Admin time slot cleanup

--- app/controllers/admin/timeslots_controller.rb
+++ app/controllers/admin/timeslots_controller.rb
@@ -3,8 +3,18 @@ class Admin::TimeslotsController < Admin::AdminController
     actions :index, :create, :new
     belongs_to :event
 
-    response_for :create do
-      redirect_to admin_events_path
+    #response_for :create do
+      #redirect_to admin_events_path
+    #end
+
+  end
+  def create
+    @timeslot = Timeslot.new(params[:timeslot], :without_protection => true)
+    if @timeslot.save
+      redirect_to admin_event_timeslots_path
+    else
+      render :new
     end
   end
+
 end
