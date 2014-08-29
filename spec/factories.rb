5490779527724301dcac52499f971860f55d3a1b
Casey Helbling <casey@softwareforgood.com>
2014-08-29 18:19:17 -0500

    Made some constants to remove the rspec warnings

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 
-@@categories = ['Design', 'Development', 'Hardware', 'Startups', 'Other']
-@@rooms = [
+CATEGORIES = ['Design', 'Development', 'Hardware', 'Startups', 'Other']
+ROOMS = [
   { :name => 'Harriet', :capacity => 100 },
   { :name => 'Calhoun', :capacity => 100 },
   { :name => 'Nokomis', :capacity => 100 },
@@ -11,22 +11,21 @@
   { :name => 'Learn', :capacity => 24 },
   { :name => 'Challenge', :capacity => 24 }
 ]
-@@slots = ["09:40", "10:40", "11:40", "13:50", "14:50", "15:50", "16:50"]
-@@session_length = 45.minutes
+SLOTS = ["09:40", "10:40", "11:40", "13:50", "14:50", "15:50", "16:50"]
+SESSION_LENGTH = 45.minutes
 
 
 FactoryGirl.define do
 
   sequence :category_name do |n|
-    @@categories[n % @@categories.length]
+    CATEGORIES[n % CATEGORIES.length]
   end
 
-
   sequence :room_name do |n|
-    @@rooms[ n % @@rooms.length][:name]
+    ROOMS[ n % ROOMS.length][:name]
   end
 
   sequence :slot_time do |n|
-    @@slots[n % @@slots.length]
+    SLOTS[n % SLOTS.length]
   end
 end
