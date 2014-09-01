e758e2b7285fcb2a630ca6a54f5bd609c2d33805
Justin Coyne <justin@curationexperts.com>
2014-09-01 12:25:17 -0500

    Clear the current_event cache when a new event is created

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -8,4 +8,13 @@ describe Event do
   it { should have_many :sessions }
   it { should have_many :timeslots }
   it { should have_many :rooms }
+
+  context "creating a new event" do
+    before { FactoryGirl.create(:event, date: 2.months.ago) }
+    it "should clear the existing current_event" do
+      expect {
+        e2 = FactoryGirl.create(:event)
+      }.to change { Event.current_event }
+    end
+  end
 end
