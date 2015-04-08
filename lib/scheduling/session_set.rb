605430cece560c3dcb2d8604e1c2c702eebb4068
Paul Cantrell <cantrell@pobox.com>
2015-04-08 01:15:42 -0500

    Improved scoring mechanism accounts for depth of session overlap

--- lib/scheduling/session_set.rb
+++ lib/scheduling/session_set.rb
@@ -1,5 +1,3 @@
-require 'set'
-
 module Scheduling
 
   # A set of sessions which can be scored against a particular schedule.
@@ -25,7 +23,8 @@ module Scheduling
     # The score if sessions are evenly distributed among all available timeslots.
     #
     def best_possible_score
-      [@ctx.timeslots.size / size.to_f, 1.0].min
+      slot_count = @ctx.timeslots.size
+      [slot_count * slot_value(size.to_f / slot_count), 1.0].min
     end
 
     # The score if everything is in the same timeslot.
@@ -37,22 +36,46 @@ module Scheduling
     def score(schedule)
       return 1 if @sessions.empty?  # prevents divide by zero below
       
-      slots_used = Set.new
-      overlaps = 0
+      slot_session_count = Hash.new(0)
+      penalty = 0
 
       @sessions.each do |session|
         slot = schedule.slot_for(session)
-        unless slots_used.add? slot
-          overlaps += 1
-        end
-        overlaps += @penalty_callback.call(slot)
+        slot_session_count[slot] += 1
+        penalty += @penalty_callback.call(slot)
       end
 
-      1 - overlaps / @sessions.size.to_f
+      satisfaction = slot_session_count.values.sum do |k|
+        slot_value(k)
+      end
+
+      satisfaction - penalty / size.to_f
     end
 
     def size
       @sessions.size
+    end
+
+  private
+
+    # Assume each attendee has some ranking of all the sessions in which they expressed interest.
+    # (Not an unreasonable assumption.) Further assume that the attendee values their sessions of interest
+    # linearly: least interested=1,2,3...N=most interested. (That's more of a stretch, but likely bears
+    # some resemblance to reality.)
+    #
+    # In each timeslot, assume the attendee will choose the session they're most interested in. We don't
+    # know attendee's actual ranking, so assume sessions of interest are distributed randomly in the schedule.
+    # The expected maximum of a slot in which there are k sessions is v_slot = k(N+1)/(k+1). The total
+    # value of all sessions is v_total = N(N+1)/2. The expected fraction of total value in one slot is
+    # thus v_slot / v_total = 2k/(N(k+1)). Sum that over all slots to get the attendee's overall satisfaction.
+    #
+    # A similar logic applies to presenters avoiding double-booking.
+    #
+    # (The old scheduling algorithm didn't take into account how deep the overlap ran within a given slot,
+    # essentially set v_slot = {1/N if any sessions of interest, 0 otherwise}.)
+    #
+    def slot_value(k)
+      2 * k / (size.to_f * (k + 1))
     end
 
   end
