add7db4b6c741e00794e85f730331f7912bbfdcd
Paul Cantrell <cantrell@pobox.com>
2015-04-07 23:55:11 -0500

    Scoring moved to SessionSet; code almost comprehensible

--- lib/scheduling/person.rb
+++ lib/scheduling/person.rb
@@ -4,16 +4,16 @@ module Scheduling
 
     def initialize(ctx, id)
       @attending  = SessionSet.new(ctx)
-      @presenting = SessionSet.new(ctx, superset: @attending)  # Presenters don't necessarily upvote their own sessions
+      @presenting = SessionSet.new(ctx,
+        superset: @attending,            # Presenters don't necessarily upvote their own sessions, but they do attend!
+        penalty_callback: ->(slot) do    # Presenters may have other scheduling constraints beside double booking
+          @timeslot_penalties[slot]
+        end)
       @timeslot_penalties = Hash.new(0)
     end
 
     # Value greater than 0 indicates desire not to present in given slot; 1 means "impossible."
     #
-    def timeslot_penalty(slot_id)
-      @timeslot_penalties[slot_id]
-    end
-
     def assign_timeslot_penalty(slot_id, penalty)
       @timeslot_penalties[slot_id] = penalty
     end
