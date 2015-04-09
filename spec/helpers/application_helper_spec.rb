28d00a54e0972f45889e5396d26277da7806b7f9
Paul Cantrell <cantrell@pobox.com>
2015-04-09 16:01:24 -0500

    Sanitize user HTML

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,6 +5,7 @@ describe ApplicationHelper do
     def normalize_space(string)
       string.
         gsub(/\s+/, ' ').        # Don't care how many spaces or what kind
+        gsub('<p></p>', ' ').    # Markdown likes to do this sometimes
         gsub(/^ +| +$/, '').     # Ignore space at beginning & end
         gsub(/> +</, '><')       # Ignore space around tags
     end
@@ -29,6 +30,38 @@ describe ApplicationHelper do
       expect(helper.markdown("foo <strong bar")).
         to eq_ignoring_space "<p>foo &lt;strong bar</p>"
     end
+
+    it 'allows some limited html formatting' do
+      html = '<p>foo <i>bar</i> and <b>baz</b><br>So <strong>there!</strong></p><ul><li>foo</li></ul>'
+      expect(helper.markdown(html)).
+        to eq_ignoring_space html
+    end
+
+    it 'allows links' do
+      expect(helper.markdown('foo <a href="http://bar.com">bar</a> baz')).
+        to eq_ignoring_space '<p>foo <a href="http://bar.com">bar</a> baz</p>'
+    end
+
+    it 'limits link attributes' do
+      expect(helper.markdown('foo <a href="http://bar.com" target="_blank" onclick="alert(1)" style="font-size: 200%" class="huge">bar</a> baz')).
+        to eq_ignoring_space '<p>foo <a href="http://bar.com">bar</a> baz</p>'
+    end
+
+    it 'allows img tags' do
+      expect(helper.markdown('foo <img src="http://bar.com"> bar')).
+        to eq_ignoring_space '<p>foo <img src="http://bar.com"> bar</p>'
+    end
+
+    it 'limits img tag attributes' do
+      expect(helper.markdown('foo <img src="http://bar.com" width=100 style="border: 2px solid red" height=200 border=7 alt="bar"> bar')).
+        to eq_ignoring_space '<p>foo <img src="http://bar.com" width="100" height="200" alt="bar"> bar</p>'
+    end
+
+    {iframe: true, script: false, nonstandard: true}.each do |tag, preserve_nested|
+      it "strips <#{tag}> tags" do
+        expect(helper.markdown("foo <#{tag}>bar</#{tag}> baz")).
+          to eq_ignoring_space "<p>foo #{'bar' if preserve_nested} baz</p>"
+      end
+    end
   end
 end
-
