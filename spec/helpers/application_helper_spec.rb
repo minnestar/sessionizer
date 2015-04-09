9681b63cf76cfb11e46ea885b979a17a467d89d2
Paul Cantrell <cantrell@pobox.com>
2015-04-09 15:27:45 -0500

    Added markdown helper spec

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  RSpec::Matchers.define :eq_ignoring_space do |expected|
+    def normalize_space(string)
+      string.
+        gsub(/\s+/, ' ').        # Don't care how many spaces or what kind
+        gsub(/^ +| +$/, '').     # Ignore space at beginning & end
+        gsub(/> +</, '><')       # Ignore space around tags
+    end
+
+    match do |actual|
+      normalize_space(actual) == normalize_space(expected)
+    end
+  end
+
+  describe "#markdown" do
+    it 'converts markdown to html' do
+      expect(helper.markdown("foo\n\n* bar\n* baz")).
+        to eq_ignoring_space "<p>foo</p><ul><li>bar</li><li>baz</li></ul>"
+    end
+  end
+end
+
