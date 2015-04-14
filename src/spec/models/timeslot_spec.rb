f96911bdaab60eac3a76b2aaac259db97cd7b224
Luke Francl <look@recursion.org>
2015-04-13 23:03:22 -0700

    Destroy PresenterTimeslotRestriction when Timeslot is destroyed.

--- src/spec/models/timeslot_spec.rb
+++ src/spec/models/timeslot_spec.rb
@@ -5,4 +5,13 @@ describe Timeslot do
   it { should validate_presence_of :starts_at }
   it { should validate_presence_of :ends_at }
   it { should belong_to :event }
+
+  context "#destroy" do
+    it "destroys associated PresenterTimeslotRestrictions" do
+      timeslot = FactoryGirl.create(:timeslot)
+      FactoryGirl.create(:presenter_timeslot_restriction, timeslot: timeslot)
+
+      expect { timeslot.destroy }.to change { PresenterTimeslotRestriction.count }.by(-1)
+    end
+  end
 end
