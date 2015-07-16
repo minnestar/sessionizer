dc8e8fdc6a447b664e5d1263e97224e32f906856
Justin Coyne <justin@curationexperts.com>
2015-07-16 14:25:48 -0500

    Store categories in a config file, not in code

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,11 +3,10 @@ class Category < ActiveRecord::Base
   has_many :sessions, :through => :categorizations
 
   def self.find_or_create_defaults
-    Category.where(id: 1).first_or_initialize.update(name: 'Design')
-    Category.where(id: 2).first_or_initialize.update(name: 'Development')
-    Category.where(id: 3).first_or_initialize.update(name: 'Hardware')
-    Category.where(id: 4).first_or_initialize.update(name: 'Startups')
-    Category.where(id: 5).first_or_initialize.update(name: 'Other')
+    cnf = YAML.load_file(File.join(Rails.root, 'config', 'categories.yml'))
+    cnf.each do |key, val|
+      Category.where(id: key).first_or_initialize.update(name: val)
+    end
   end
 
 end
