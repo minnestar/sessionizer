c912d1770b379af8ef8e728b0bdf187ffa4ad343
Paul Cantrell <cantrell@pobox.com>
2016-04-17 11:12:33 -0500

    Random sessions on HP; most recent first in index

--- src/app/controllers/pages_controller.rb
+++ src/app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @current_event = Event.current_event
-    @recent_sessions = @current_event ? @current_event.sessions.limit(10).order('created_at desc') : []
+    @recent_sessions = @current_event ? @current_event.sessions.limit(10).random_order : []
 
     @categories = Category.all.order('id')
   end
