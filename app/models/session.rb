f5b97c0af6b68025ffaa43b658a5c8fc59581174
Paul Cantrell <cantrell@pobox.com>
2016-04-17 11:12:33 -0500

    Random sessions on HP; most recent first in index

--- app/models/session.rb
+++ app/models/session.rb
@@ -19,7 +19,9 @@ class Session < ActiveRecord::Base
 
   scope :with_attendence_count, -> { select('*').joins("LEFT OUTER JOIN (SELECT session_id, count(id) AS attendence_count FROM attendances GROUP BY session_id) AS attendence_aggregation ON attendence_aggregation.session_id = sessions.id") }
 
-  scope :for_current_event, lambda { where(event_id: Event.current_event.id) }
+  scope :for_current_event, -> { where(event_id: Event.current_event.id) }
+
+  scope :random_order, -> { order('random()') }  # Slow, but fast enough when only ~100 rows
 
   validates_presence_of :description
   validates_presence_of :event_id
