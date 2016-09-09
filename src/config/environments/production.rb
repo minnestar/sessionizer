416fa9f4088f02026f0e81929f5d19c239fb1caf
Erin Fahy <efahy@stanford.edu>
2016-09-08 17:07:22 -0700

    correct no reply address

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: "no-reply@minnestar.org" }
+  config.action_mailer.default_options = {from: "no-reply@projecthydra.org" }
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
