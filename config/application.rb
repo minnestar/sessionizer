96081643ecc422e11b39319eac4ca6af4606d1ee
Justin Coyne <jcoyne@justincoyne.com>
2016-10-11 23:31:07 -0500

    restore autoload of lib/

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Sessionizer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.autoload_paths += [config.root.join('lib')] 
   end
 end
