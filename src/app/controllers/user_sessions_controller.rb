297f34f168a49c707bf09f4b856c045687ace9c2
Paul Cantrell <cantrell@pobox.com>
2017-03-23 21:30:48 -0500

    Prompt users to log in to see votes on schedule

--- src/app/controllers/user_sessions_controller.rb
+++ src/app/controllers/user_sessions_controller.rb
@@ -1,5 +1,8 @@
 class UserSessionsController < ApplicationController
   def new
+    if params[:after_login]
+      session[:after_login] = params[:after_login]
+    end
     @participant_session = ParticipantSession.new
   end
 
@@ -7,7 +10,8 @@ class UserSessionsController < ApplicationController
     @participant_session = ParticipantSession.new(participant_session_params)
     if @participant_session.save
       flash[:notice] = "You're logged in. Welcome back."
-      redirect_to root_path
+      redirect_to session[:after_login] || root_path
+      session.delete(:after_login)
     else
       flash.now[:error] = "Sorry, couldn't find that participant. Try again, or sign up to register a new account."
       render :action => :new
