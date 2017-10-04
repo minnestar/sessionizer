93ccc62a69dcab824c9e1b2b4936111ab3093bc0
Justin Coyne <jcoyne@justincoyne.com>
2017-10-04 12:20:53 -0500

    Remove the implied Lukeness from the factory session
    If Lukeness is required, make it explicit.  This fixes a problem when
    you try to use the session factory twice and it complains that Luke's
    email is already taken.

--- src/spec/factories/session.rb
+++ src/spec/factories/session.rb
@@ -1,13 +1,10 @@
 
 FactoryGirl.define do
-
   factory :session do
-
-    title "Stuff about things"
-    description "whatever"
-    participant { create :luke }
+    title 'Stuff about things'
+    description 'whatever'
+    participant
     association :event
     association :room
-
   end
 end
