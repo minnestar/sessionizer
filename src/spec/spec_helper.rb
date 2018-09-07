a08e5b24c08ed4144bda7b0c4a0fdc21c96afeba
Jason Hsu <jhsu802701@jasonhsu.com>
2018-09-07 05:06:17 +0000

    Edited src/spec/spec_helper.rb to allow SimpleCov to run in the local environment, not just in TravisCI

--- src/spec/spec_helper.rb
+++ src/spec/spec_helper.rb
@@ -1,13 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 
+require 'simplecov'
 if ENV['CI']
-  require 'simplecov'
   require 'coveralls'
   SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-  SimpleCov.start :rails do
-    add_filter "/spec/"
-  end
+end
+SimpleCov.start :rails do
+  add_filter "/spec/"
 end
 
 require File.expand_path("../../config/environment", __FILE__)
