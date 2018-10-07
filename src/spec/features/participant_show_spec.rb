388d793098b1d54566d99dd8a1a4a54caaa0d47a
Jason Hsu <jhsu802701@jasonhsu.com>
2018-10-07 05:10:35 +0000

    Corrected typo on participant show page

--- src/spec/features/participant_show_spec.rb
+++ src/spec/features/participant_show_spec.rb
@@ -9,7 +9,7 @@ feature 'View participant profile' do
       expect(page).to have_selector('h1', text: 'Luke Francl')
       expect(page).to have_content 'the man with the master plan'
       expect(page).to have_content "This person isn't presenting any sessions."
-      expect(page).to have_content "This person hasn't expressed interested in any sessions yet."
+      expect(page).to have_content "This person hasn't expressed interest in any sessions yet."
     end
   end
 end
