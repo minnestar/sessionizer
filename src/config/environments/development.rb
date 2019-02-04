d049f831dd85199af18743d31c1ba0c9e9fce686
Jason Hsu <jhsu802701@jasonhsu.com>
2019-02-04 19:48:47 +0000

    Made modifications to resolve merge conflict

--- src/config/environments/development.rb
+++ src/config/environments/development.rb
@@ -1,6 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # BEGIN: needed for email confirmations
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  # END: needed for email confirmations
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
