b23534b7a1853505e62b9b3d8cf96f07a9d0d006
Cory Preus <cory@unsay.com>
2024-04-12 22:05:30 -0500

    nit: No need for quotes around proper nouns.

--- src/app/controllers/presentations_controller.rb
+++ src/app/controllers/presentations_controller.rb
@@ -12,11 +12,11 @@ class PresentationsController < ApplicationController
     participant = Participant.find(params[:id])
 
     if participant.nil?
-      flash[:error] = "Sorry, no presenter named '#{params[:name]}' was found. Please try again."
+      flash[:error] = "Sorry, no presenter named #{params[:name]} was found. Please try again."
       redirect_to session_presentations_path(@session)
       return
     elsif participant.signed_code_of_conduct_for_current_event? == false
-      flash[:error] = "Sorry, '#{params[:name]}' hasn't signed the Code of Conduct for this event."
+      flash[:error] = "Sorry, #{params[:name]} hasn't signed the current Code of Conduct."
       redirect_to session_presentations_path(@session)
       return
     end
