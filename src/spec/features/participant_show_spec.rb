0cf8c9fd0e3b018f8ed71d563d3b5c1bc69c8a9f
Paul Cantrell <cantrell@pobox.com>
2024-06-09 12:03:24 -0500

    Fixed participant show spec setup
    (for_current_event should always return a collection, never nil)

--- src/spec/features/participant_show_spec.rb
+++ src/spec/features/participant_show_spec.rb
@@ -2,9 +2,12 @@ require "spec_helper"
 
 feature 'View participant profile' do
   context 'With an authenticated user' do 
+    background do
+      create(:event, :full_event)
+    end
     let(:luke) { create(:luke) }
 
-    scenario 'Can view participant profile when there are no events or sessions' do
+    scenario 'Can view participant profile when there are no sessions' do
       visit participant_path(luke)
       expect(page).to have_selector('h1', text: 'Luke Francl')
       expect(page).to have_content 'the man with the master plan'
