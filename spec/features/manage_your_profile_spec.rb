32e687973cebf52327426c0cc31129db419810ac
Matt Decuir <code@experimatt.com>
2025-02-22 10:53:43 -0600

    Revert accidental changes

--- spec/features/manage_your_profile_spec.rb
+++ spec/features/manage_your_profile_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 feature "Manage a user profile" do
 
-  context "As an authenticated user without a confirmed email" do
+  context "As an authenticated user" do
     let(:joe) { create(:joe, email_confirmed_at: Time.now) }
     background do
       create(:event)
@@ -23,5 +23,6 @@ feature "Manage a user profile" do
       expect(page).to have_content bio
     end
   end
+
 end
 
