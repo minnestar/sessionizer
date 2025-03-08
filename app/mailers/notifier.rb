be4cf6d5db562e89c0775eccd7b78d32de600aa8
Matt Decuir <code@experimatt.com>
2025-03-08 11:34:13 -0600

    Update Confirmation email subject and content

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -10,6 +10,6 @@ class Notifier < ActionMailer::Base
 
   def participant_email_confirmation(participant)
     @participant = participant
-    mail(to: participant.email, subject: "Please confirm your email")
+    mail(to: participant.email, subject: "Confirm your email for Minnebar")
   end
 end
