0bece08af1ccfa2dba42deba4f309e0462fdee7b
Matt Decuir <code@experimatt.com>
2025-03-12 21:12:24 -0500

    Remove problematic admin create session endpoint

--- app/controllers/admin/legacy/sessions_controller.rb
+++ app/controllers/admin/legacy/sessions_controller.rb
@@ -11,38 +11,11 @@ class Admin::Legacy::SessionsController < Admin::Legacy::AdminController
     respond_with(@session)
   end
 
-  def new
-    respond_with(@session)
-  end
-
   def update
     if @session.update(session_params)
       redirect_to admin_legacy_sessions_path
     else
       render :edit
-    end
-  end
-
-  def build_presenter
-    name = params[:session].delete(:name)
-    # find exact match by name
-    Participant.where(name: name).first_or_initialize do |p|
-      p.save(validate: false) if p.new_record?
-    end
-  end
-
-  def create
-    @session.participant = build_presenter
-    @session.attributes = session_params
-    @session.event = Event.current_event
-    @session.timeslot_id = params[:session][:timeslot_id]
-    @session.room_id = params[:session][:room_id]
-
-    if @session.save
-      flash[:notice] = "Presentation added"
-      redirect_to admin_legacy_sessions_path
-    else
-      render :new
     end
   end
 
