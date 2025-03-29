27b085c3c365f8af677b14dff524964cfc0fb66d
Matt Decuir <code@experimatt.com>
2025-03-29 11:22:30 -0500

    Use dynamic meta description content from current event (#355)
    * Use dynamic meta description content from current event
    
    * Add fallback in case Event.current_event doesn't exist

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,8 +28,17 @@ module ApplicationHelper
     Nokogiri::HTML::DocumentFragment.parse(html.scrub).to_html
   end
 
+  def default_meta_description
+    if Event.current_event
+      # TODO: Add venue field to Event model and use it here instead of hardcoding "Best Buy HQ"
+      "#{Event.current_event.name} | #{Event.current_event.date.strftime("%B %e, %Y")} | Best Buy HQ"
+    else
+      "Minnebar"
+    end
+  end
+
   def meta_description
-    content_for?(:meta_description) ? content_for(:meta_description) : 'Minnebar'
+    content_for?(:meta_description) ? content_for(:meta_description) : default_meta_description
   end
 
   def sanitize_html(html)
