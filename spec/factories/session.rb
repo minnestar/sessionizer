f6015412ce2dca801005563ca66fc88970fee813
Matt Decuir <code@experimatt.com>
2025-03-30 11:33:22 -0500

    Fix bug in session factory

--- spec/factories/session.rb
+++ spec/factories/session.rb
@@ -1,12 +1,15 @@
-
 FactoryBot.define do
   factory :session do
     sequence :title do |n|
-      " Session #{n}"
+      "Session #{n}"
     end
     description { 'whatever' }
     participant
     association :event
-    association :room
+
+    # ensure room is associated with the same event as the session
+    after(:build) do |session|
+      session.room = session.event.rooms.first || create(:room, event: session.event)
+    end
   end
 end
