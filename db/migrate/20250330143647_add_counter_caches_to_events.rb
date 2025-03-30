b94f78cfd2cfd3aafa7802edb85d9b55ee7c2496
Matt Decuir <code@experimatt.com>
2025-03-30 11:37:35 -0500

    Add reset_counters to events counter cache migration

--- db/migrate/20250330143647_add_counter_caches_to_events.rb
+++ db/migrate/20250330143647_add_counter_caches_to_events.rb
@@ -3,5 +3,13 @@ class AddCounterCachesToEvents < ActiveRecord::Migration[7.1]
     add_column :events, :sessions_count, :integer, default: 0
     add_column :events, :rooms_count, :integer, default: 0
     add_column :events, :timeslots_count, :integer, default: 0
+
+    reversible do |dir|
+      dir.up do
+        say_with_time "Updating counter caches..." do
+          Event.find_each { |event| Event.reset_counters(event.id, :sessions, :rooms, :timeslots) }
+        end
+      end
+    end
   end
 end
