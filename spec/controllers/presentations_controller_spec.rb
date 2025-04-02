f06eb587a7c0fe69afa7bd59e9c289956f1c5c08
Matt Decuir <code@experimatt.com>
2025-04-01 21:11:47 -0500

    Attempt #2 to fix flaky presentations controller spec

--- spec/controllers/presentations_controller_spec.rb
+++ spec/controllers/presentations_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe PresentationsController do
-  let(:session) { create(:session) }
-  let(:participant) { create(:joe) }
+  let!(:session) { create(:session) }
+  let!(:participant) { create(:joe) }
 
   describe "#index" do
     it "should be successful" do
@@ -27,7 +27,7 @@ describe PresentationsController do
 
         expect {
           post :create, params: { session_id: session, id: participant.id }
-        }.to change { session.presentations.count }.by(1)
+        }.to change { session.reload.presentations.count }.by(1)
         expect(response).to redirect_to session_presentations_path(session)
         expect(flash[:notice]).to eq "Presenter added."
       end
@@ -41,16 +41,10 @@ describe PresentationsController do
 
     context "when the user is found by name" do
       it "should be successful when the user has signed the code of conduct" do
-        # 2025-03-30: added reloads below in an attempt to prevent this test from flaking
-        participant.reload
-
         CodeOfConductAgreement.create!({
           participant_id: participant.id,
           event_id: Event.current_event.id,
         })
-
-        # reload to ensure session is fresh
-        session.reload
 
         expect {
           post :create, params: { session_id: session, name: participant.name }
@@ -70,7 +64,7 @@ describe PresentationsController do
       it "should set a flash message" do
         expect {
           post :create, params: { session_id: session, name: 'Grace Hopper' }
-        }.not_to change { session.presentations.count }
+        }.not_to change { session.reload.presentations.count }
         expect(flash[:error]).to eq "Sorry, no presenter matching 'Grace Hopper' was found. Please try again."
         expect(response).to redirect_to session_presentations_path(session)
       end
@@ -80,7 +74,7 @@ describe PresentationsController do
       it "should set a flash message" do
         expect {
           post :create, params: { session_id: session, id: 0 }
-        }.not_to change { session.presentations.count }
+        }.not_to change { session.reload.presentations.count }
         expect(flash[:error]).to eq "There was an error adding the presenter. Please try again. If this keeps happening, please contact support@minnestar.org."
         expect(response).to redirect_to session_presentations_path(session)
       end
